a971e474a420681e506c7ff6018536481fe3380c
Björn Victor <bjorn@victor.se>
2020-07-01 11:25:20 +0200

    Updated demo programs

---
+++ finger.py
@@ -0,0 +1,150 @@
+# Chaosnet client for NAME protocol (what is otherwise known as finger, which is a different protocol on Chaosnet)
+# Demonstrates the APIs for the NCP of cbridge: both the simpler stream protocol and the packet protocol.
+
+import socket, io
+import sys, subprocess, threading
+import re, string
+from datetime import datetime
+from enum import IntEnum, auto
+
+# The directories of these need to match the "socketdir" ncp setting in cbridge.
+stream_address = '/tmp/chaos_stream'
+packet_address = '/tmp/chaos_packet'
+# -d
+debug = False
+packetp = False
+
+# Chaos packet opcodes
+class Opcode(IntEnum):
+    RFC = 1
+    OPN = auto()
+    CLS = auto()
+    FWD = auto()
+    ANS = auto()
+    SNS = auto()
+    STS = auto()
+    RUT = auto()
+    LOS = auto()
+    LSN = auto()
+    MNT = auto()
+    EOF = auto()
+    UNC = auto()
+    BRD = auto()
+    DAT = 0o200
+    DWD = 0o300
+
+# Construct a 4-byte packet header for chaos_packet connections
+def packet_header(opc, plen):
+    return bytes([opc, 0, plen & 0xff, int(plen/256)])
+
+def get_socket(address):
+    # Create a Unix socket
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+    # Connect the socket to the port where the server is listening
+    try:
+        sock.connect(address)
+        return sock
+    except socket.error as msg:
+        print(msg)
+        sys.exit(1)
+
+def stream_conn(addr, host, contact, args=[], timeout=None):
+    if debug:
+        print('stream_conn',addr,host,contact,args,timeout)
+    sock = get_socket(addr)
+    if isinstance(host, int):
+        host = "{:o}".format(host)
+
+    if timeout is not None:
+        d = bytes(("[timeout={:d}] {} {}"+" {}"*len(args)).format(timeout,host,contact,*args),"ascii")
+    else:
+        d = bytes(("{} {}"+" {}"*len(args)).format(host,contact,*args),"ascii")
+    if packetp:
+        if debug:
+            print('packet',packet_header(Opcode.RFC, len(d))+d)
+        sock.sendall(packet_header(Opcode.RFC, len(d))+d)
+    else:
+        if debug:
+            print(b"stream: RFC "+d)
+        sock.sendall(b"RFC "+d)
+    try:
+        # Receive max 488 data bytes, 16 to fit "ANS 488\r\n" (so 9 would be enough)
+        data = sock.recv(488+16)
+        if packetp:
+            if data[0] == Opcode.ANS:
+                dlen = data[2] + data[3]*256
+                return data[4:dlen+4]
+            elif data[0] == Opcode.OPN:
+                return sock
+            elif timeout is not None and data[0] == Opcode.LOS and data[4:].startswith(b"Connection timed out"):
+                return None
+            else:
+                print("Unexpected response {} len {} ({})".format(Opcode(data[0]).name, data[2] + data[3]*256, data[4:]))
+        else:
+            eol = data.find(b'\n')
+            resp = data[:eol].split(b' ')
+            if resp[0] == b"ANS":
+                dstart = eol+1
+                dlen = int(resp[1])
+                return data[dstart:dstart+dlen]
+            elif resp[0] == b"OPN":
+                return sock
+            elif timeout is not None and resp[0] == "LOS" and data[eol+1:].startswith(b"Connection timed out"):
+                return None
+            else:
+                print("Unexpected response {} len {}".format(resp[0],len(data)))
+    except socket.error:
+        return None
+
+def finger(host,user=None):
+    sock = stream_conn(packet_address if packetp else stream_address,host,"NAME",args=(['/W',user] if user is not None else []))
+    if sock is not None:
+        data = []
+        while True:
+            if len(data) == 0:
+                data = sock.recv(488+4)
+            if len(data) == 0:
+                break
+            if packetp:
+                opc = data[0]
+                if data[1] != 0:
+                    print("Bad header - should be zero:",data[1])
+                lenth = data[2] + (data[3]*256)
+                if debug:
+                    print("@@@@ opc {} len {}".format(Opcode(opc).name, lenth))
+                    if opc == Opcode.DAT and lenth == 4:
+                        print("{!r}".format(data[4:lenth+4]))
+                if len(data) < lenth+4:
+                    if debug:
+                        print("@@@@ reading {} more data".format(lenth+4-len(data)))
+                    more = sock.recv(lenth+4-len(data))
+                    data = data+more
+                print("{!s}".format(str(data[4:lenth+4].translate(bytes.maketrans(b'\211\215\214\212',b'\t\n\f\r')),"utf8")),end='')
+                if opc != Opcode.DAT and debug and lenth > 0:
+                    print()
+                data = data[lenth+4:]
+            else:
+                print("{!s}".format(str(data.translate(bytes.maketrans(b'\211\215\214\212',b'\t\n\f\r')),"utf8")), end='')
+                data = []
+    else:
+        print("Connection failed")
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Chaosnet finger/name')
+    parser.add_argument("-d",'--debug',dest='debug',action='store_true',
+                            help='Turn on debug printouts')
+    parser.add_argument("--packet",dest='packetp',action='store_true',
+                            help='Use packet mode (chaos_seqpacket) instead of plain stream')
+    parser.add_argument("host", help='The host to check')
+    parser.add_argument("user", nargs="?", help='User to check')
+    args = parser.parse_args()
+
+    if args.packetp:
+        packetp = True
+    if args.debug:
+        debug = True
+
+    finger(args.host, args.user)
+
--- named.py
+++ named.py
@@ -1,24 +1,52 @@
 # Chaosnet server for NAME protocol (what is otherwise known as finger, which is a different protocol on Chaosnet)
+# Demonstrates the APIs for the NCP of cbridge: both the simpler stream protocol and the packet protocol.
+
 import socket, io
 import sys, subprocess, threading
 import re, string
+from datetime import datetime
+from enum import IntEnum, auto
 
+# Default finger program. Should accept '-s' and '-l' as switches, for best effect.
+# Can be set with the --program switch.
 finger_program = "/usr/bin/finger"
+# The directories of these need to match the "socketdir" ncp setting in cbridge.
 server_address = '/tmp/chaos_stream'
+packet_address = '/tmp/chaos_packet'
+# -d
 debug = False
+# --packet
+packetp = False
 
-def switch_interpreter(x):
-    # Translate /W to -l
-    if x.startswith(b'/'):
-        if x == b'/W':
-            return b'-l'
-        # ignore other switches
-    else:
-        return x
+# Chaos packet opcodes
+class Opcode(IntEnum):
+    RFC = 1
+    OPN = auto()
+    CLS = auto()
+    FWD = auto()
+    ANS = auto()
+    SNS = auto()
+    STS = auto()
+    RUT = auto()
+    LOS = auto()
+    LSN = auto()
+    MNT = auto()
+    EOF = auto()
+    UNC = auto()
+    BRD = auto()
+    DAT = 0o200
+    DWD = 0o300
+
+# Construct a 4-byte packet header for chaos_packet connections
+def packet_header(opc, plen):
+    return bytes([opc, 0, plen & 0xff, int(plen/256)])
 
 def give_finger_response(sock, args):
     # Run finger and capture the output
-    pargs = [finger_program,'-s']+list(filter(lambda x: x is not None, map(switch_interpreter, args)))
+    pargs = [finger_program,'-s']+\
+        list(filter(lambda x: x is not None,
+                        map(lambda a: b'-l' if a == b'/W' else a if not a.startswith(b'/') else None,
+                                args)))
     if debug:
         print("Running {!s}".format(pargs),file=sys.stderr)
     r = subprocess.run(pargs, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
@@ -26,7 +54,24 @@ def give_finger_response(sock, args):
     # Translate to lispm characters, kind of - should also handle Unicode de-translation?
     rout = r.stdout.translate(bytes.maketrans(b'\t\n\f\r',b'\211\215\214\212'))
     # Send the response, and close the socket
-    sock.sendall(rout)
+    if packetp:
+        # Send it in packet chunks
+        for i in range(0, len(rout), 488):
+            chunk = rout[i : i+488]
+            clen = len(chunk)
+            hdr = packet_header(Opcode.DAT, clen)
+            if debug:
+                print("sending chunk len {} header {!r}".format(clen,hdr))
+            sock.sendall(bytes(hdr)+chunk)
+        # Finish with an EOF, to make sure everything is read before closing
+        if debug:
+            print("sending EOF")
+        sock.sendall(packet_header(Opcode.EOF, 0))
+    else:
+        if debug:
+            print("sending data len {}".format(len(rout)))
+        # Nothing special is needed, just write the data. EOF is automatic in this case.
+        sock.sendall(rout)
     sock.close()
 
 def name_server():
@@ -35,28 +80,45 @@ def name_server():
 
     # Connect the socket to the port where the server is listening
     try:
-        sock.connect(server_address)
+        if packetp:
+            sock.connect(packet_address)
+        else:
+            sock.connect(server_address)
     except socket.error as msg:
         print(msg)
         sys.exit(1)
 
     # Set up a listener for NAME
-    sock.sendall(b'LSN NAME')
+    if packetp:
+        sock.sendall(packet_header(Opcode.LSN, 4) + b"NAME")
+    else:
+        sock.sendall(b'LSN NAME')
 
     try:
         # Get a request from the socket
-        data = sock.recv(128)
-        # Strip newlines, split at blanks
-        args = data.rstrip().split(b' ')
-        if args[0] == b"RFC":
-            if debug:
-                print('got RFC from {!s} with args {!r}'.format(args[1],args[2:]))
-            # Accept the request
-            sock.sendall(b"OPN\r\n")
-            t = threading.Thread(target=give_finger_response,args=(sock, args[2:])).start()
+        data = sock.recv(488+16)
+        if packetp:
+            if data[0] == Opcode.RFC:
+                args = data[4:].rstrip().split(b' ')
+                if debug:
+                    print(datetime.now(),'got RFC from {!s} with args {!r}'.format(args[0],args[1:]))
+                sock.sendall(packet_header(Opcode.OPN, 0))
+                t = threading.Thread(target=give_finger_response,args=(sock, args[1:])).start()
+            else:
+                sock.sendall(packet_header(Opcode.CLS, len("Bad request"))+b"Bad request")
+                sock.close()
         else:
-            sock.sendall(b'CLS Bad request\r\n')
-            sock.close()
+            # Strip newlines, split at blanks
+            args = data.rstrip().split(b' ')
+            if args[0] == b"RFC":
+                if debug:
+                    print(datetime.now(),'got RFC from {!s} with args {!r}'.format(args[1],args[2:]))
+                # Accept the request
+                sock.sendall(b"OPN\r\n")
+                t = threading.Thread(target=give_finger_response,args=(sock, args[2:])).start()
+            else:
+                sock.sendall(b'CLS Bad request\r\n')
+                sock.close()
     except socket.error as msg:
         print(msg)
 
@@ -67,6 +129,8 @@ if __name__ == '__main__':
                             help='Turn on debug printouts')
     parser.add_argument("-p",'--program',nargs=1,dest='program',
                             help='The program to run to get finger information')
+    parser.add_argument("--packet",dest='packetp',action='store_true',
+                            help='Use packet mode (chaos_seqpacket) instead of plain stream')
     args = parser.parse_args()
     if args.program:
         finger_program = args.program[0]
@@ -74,6 +138,8 @@ if __name__ == '__main__':
     if args.debug:
         print("Turning debug on")
         debug = True
+    if args.packetp:
+        packetp = True
 
     # Handle many requests
     while True:
