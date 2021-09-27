0c0d6383073ef56f5b74060360e3af7a4d88a688
Björn Victor <bjorn@victor.se>
2021-09-27 10:19:17 +0200

    Better test in startup script

--- cbridge-starter.sh
+++ cbridge-starter.sh
@@ -4,15 +4,14 @@
 # This is ridiculous.
 # I would have expected After=network.online ns-lookup.target to take care of this,
 # but without it, any DNS name parsing might fail with "temporary errors" causing config parsing to fail,
-# since IP only has a temporary 169.x.y.z address, 
 # and cbridge will not start.
 i=0
-while /sbin/ip -o addr show dev eth0 | /bin/grep -q 'inet 169'; do
+while ! /usr/bin/host -t a router.chaosnet.net > /dev/null; do
     # /sbin/ip -o addr show dev eth0 | grep 'inet ';
     i=$((i+1));
     sleep 2;
 done
 if [ $i -gt 0 ]; then
-   echo "Waited $i rounds for network to come up";
+   echo "Waited $i rounds for DNS to come up";
 fi
 exec ./cbridge
