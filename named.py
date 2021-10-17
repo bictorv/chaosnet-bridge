6cef8f5989cf6f297d9715e2a7fa05e743e4e1f9
Björn Victor <bjorn@victor.se>
2021-10-17 18:01:49 +0200

    Add copyright notices.

--- named.py
+++ named.py
@@ -1,5 +1,18 @@
+# Copyright © 2021 Björn Victor (bjorn@victor.se)
 # Chaosnet server for NAME protocol (what is otherwise known as finger, which is a different protocol on Chaosnet)
 # Demonstrates the APIs for the NCP of cbridge: both the simpler stream protocol and the packet protocol.
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
 
 import socket, io, time
 import sys, subprocess, threading
