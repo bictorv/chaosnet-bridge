6fb8611c2a09c44b6d5332008ec28c3c7d35e06d
Björn Victor <bjorn@victor.se>
2023-10-11 12:58:31 +0200

    Check that Chaosnet DNS is available
    If TLS is used, Chaosnet DNS needs to be available to check our cert. So wait for it to be available.

--- cbridge-starter.sh
+++ cbridge-starter.sh
@@ -5,6 +5,13 @@
 # I would have expected After=network.online ns-lookup.target to take care of this,
 # but without it, any DNS name parsing might fail with "temporary errors" causing config parsing to fail,
 # and cbridge will not start.
+
+# Maybe cd before starting cbridge
+if [ $# -gt 0 ]; then
+    cd $1
+fi
+
+# Check that DNS services are up
 i=0
 while ! /usr/bin/host -t a router.chaosnet.net > /dev/null; do
     # /sbin/ip -o addr show dev eth0 | grep 'inet ';
@@ -14,9 +21,28 @@ done
 if [ $i -gt 0 ]; then
    echo "Waited $i rounds for DNS to come up";
 fi
-# Maybe cd before starting cbridge
-if [ $# -gt 0 ]; then
-    cd $1
+
+# Now wait for the Chaosnet DNS server to be available.
+# This is needed for TLS cert checking.
+i=0
+srv=""
+if grep -q -E '^tls ' cbridge.conf; then
+    # See if some special server is configured
+    if grep -q -E '^dns .*server ' cbridge.conf; then
+	srv=`grep ^dns cbridge.conf | sed -e 's/dns .*server //' | sed -e 's/ .*//'`
+    fi
+    if [ "X$srv" = "X" ]; then
+	# Default DNS server
+	srv=dns.chaosnet.net
+    fi
+    while ! /usr/bin/host -t a -c ch router.chaosnet.net $srv > /dev/null; do
+	i=$((i+1));
+	sleep 2;
+    done
+    if [ $i -gt 0 ]; then
+	echo "Waited $i rounds for Chaosnet DNS to come up";
+    fi
 fi
+
 ./cbridge-starter-services.sh &
 exec ./cbridge
